61395b6a0b2010431179d55f07afd4a6755d4c8f
Sam Page <sampage6@gmail.com>
2022-01-26 14:47:52 -0600

    added functionality to shrink nav on scroll

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+    window.addEventListener("scroll", rollup)
+});
 
-
-
-
-
-
-const header = document.querySelector('header')
-
-const headerScroll = () => {
-    if ("scrolling") {
-    header.setAttribute('style', 'display: none;')
+function rollup(ev) {
+    console.log(window.scrollY);
+    if (window.scrollY > 115) {
+        document.body.classList.remove("med");
+        document.body.classList.add("up");
+    } else if (window.scrollY > 60) {
+        document.body.classList.add("med");
+    } else {
+        document.body.classList.remove("med");
+        document.body.classList.remove("up")
     }
 }
 
+// const debounce = func => {
+//  let timer;
+//  return event => {
+//      if (timer) {
+//          clearTimeout(timer);
+//      }
+//      timer = setTimeout(func, 10, event)
+//  }
+// }
 
-window.addEventListener('scroll', headerScroll)
+
+
+
+// const header = document.querySelector('header')
+
+// const headerScroll = () => {
+//     if ("scrolling") {
+//     header.setAttribute('style', 'display: none;')
+//     }
+// }
+
+
+// window.addEventListener('scroll', headerScroll)
